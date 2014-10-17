10ef6e289afd90ae7185dc89a9e3ab7d844eba48
Ali Yeysides <ali.yeysides@gmail.com>
2014-10-17 11:13:37 -0400

    added sources

---
+++ psuedo.rb
@@ -0,0 +1,13 @@
+=begin
+As a user I want to:
+  set number of players
+  start the game -- player n
+  pick a flashcard
+  guess an answer - 2 chances
+  want game to end at 5 points
+  see the score
+  exit the game
+=end
+
+
+
